834327e5939a38fe843ba59f11e8b56e7690ebfc
umako_osone <umako0720@gmail.com>
2023-06-19 21:44:08 +0900

    0618

--- src/js/script.js
+++ src/js/script.js
@@ -5,13 +5,13 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     $(function () {
         $('.js-hamburger').on('click', function() {
             $('.js-hamburger').toggleClass('is-active');
-            $('.js-header').toggleClass('is-active');
+            $('.js-header__inner').toggleClass('is-active');
             $('.js-drawer').fadeToggle();
             $('body').toggleClass('no-scroll');
         });
         $('.js-drawer').on('click', function() {
             $('.js-hamburger').removeClass('is-active');
-            $('.js-header').removeClass('is-active');
+            $('.js-header__inner').removeClass('is-active');
             $('.js-drawer').fadeOut();
             $('body').removeClass('no-scroll');
         });
@@ -31,6 +31,23 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         speed: 3000,
         autoplay: {
             delay: 3000,
+        },
+    });
+
+    // スワイパー
+    const campaignSwiper = new Swiper('.campaign-swiper', {
+        slidesPerView: 'auto',
+        loop: true,
+        speed: 3000,
+        slideToClickedSlide: true,
+        spaceBetween: 24,
+        autoplay: {
+            delay: 3000,
+        },
+        breakpoints: {
+            768: {
+                spaceBetween: 40,
+            }
         },
     });
 
