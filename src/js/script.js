9998da1e5bf2eed21dfcb7dab9be177cd6ba8e66
umako_osone <umako0720@gmail.com>
2023-06-25 20:22:56 +0900

    0625

--- src/js/script.js
+++ src/js/script.js
@@ -26,13 +26,13 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     });
 
     // スワイパー
-    const fvSwiper = new Swiper('.fv-swiper', {
-        loop: true,
-        speed: 3000,
-        autoplay: {
-            delay: 3000,
-        },
-    });
+    // const fvSwiper = new Swiper('.fv-swiper', {
+    //     loop: true,
+    //     speed: 3000,
+    //     autoplay: {
+    //         delay: 3000,
+    //     },
+    // });
 
     // スワイパー
     const campaignSwiper = new Swiper('.campaign-swiper', {
@@ -52,69 +52,69 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     });
 
     // 背景色の後に画像
-    // インフォメーション
-    $('.information__img').each(function(){
-        $(this).append('<div class="color"></div>');
-        var color = $(this).find($('.color'));
-        var img = $(this).find('img');
-        var counter = 0;
+    // // インフォメーション
+    // $('.information__img').each(function(){
+    //     $(this).append('<div class="color"></div>');
+    //     var color = $(this).find($('.color'));
+    //     var img = $(this).find('img');
+    //     var counter = 0;
 
-        img.css('opacity','0');
-        color.css('width','0%');
-        //inviewを使って背景色が画面に現れたら処理をする
-        color.on('inview', function(){
-            if(counter == 0){
-            $(this).delay(200).animate({'width':'100%'},700,function(){
-                img.css('opacity','1');
-                $(this).css({'left':'0' , 'right':'auto'});
-                $(this).animate({'width':'0%'},700);
-            });
-                counter = 1;
-            };
-        });
-    });
+    //     img.css('opacity','0');
+    //     color.css('width','0%');
+    //     //inviewを使って背景色が画面に現れたら処理をする
+    //     color.on('inview', function(){
+    //         if(counter == 0){
+    //         $(this).delay(200).animate({'width':'100%'},700,function(){
+    //             img.css('opacity','1');
+    //             $(this).css({'left':'0' , 'right':'auto'});
+    //             $(this).animate({'width':'0%'},700);
+    //         });
+    //             counter = 1;
+    //         };
+    //     });
+    // });
     // ボイス
-    $('.voice-card__img').each(function(){
-        $(this).append('<div class="color"></div>');
-        var color = $(this).find($('.color'));
-        var img = $(this).find('img');
-        var counter = 0;
+    // $('.voice-card__img').each(function(){
+    //     $(this).append('<div class="color"></div>');
+    //     var color = $(this).find($('.color'));
+    //     var img = $(this).find('img');
+    //     var counter = 0;
 
-        img.css('opacity','0');
-        color.css('width','0%');
-        //inviewを使って背景色が画面に現れたら処理をする
-        color.on('inview', function(){
-            if(counter == 0){
-            $(this).delay(200).animate({'width':'100%'},700,function(){
-                img.css('opacity','1');
-                $(this).css({'left':'0' , 'right':'auto'});
-                $(this).animate({'width':'0%'},700);
-                    });
-                counter = 1;
-            };
-        });
-    });
+    //     img.css('opacity','0');
+    //     color.css('width','0%');
+    //     //inviewを使って背景色が画面に現れたら処理をする
+    //     color.on('inview', function(){
+    //         if(counter == 0){
+    //         $(this).delay(200).animate({'width':'100%'},700,function(){
+    //             img.css('opacity','1');
+    //             $(this).css({'left':'0' , 'right':'auto'});
+    //             $(this).animate({'width':'0%'},700);
+    //                 });
+    //             counter = 1;
+    //         };
+    //     });
+    // });
     // プライス
-    $('.price__img-pc').each(function(){
-        $(this).append('<div class="color"></div>');
-        var color = $(this).find($('.color'));
-        var img = $(this).find('img');
-        var counter = 0;
+    // $('.price__img-pc').each(function(){
+    //     $(this).append('<div class="color"></div>');
+    //     var color = $(this).find($('.color'));
+    //     var img = $(this).find('img');
+    //     var counter = 0;
 
-        img.css('opacity','0');
-        color.css('width','0%');
-        //inviewを使って背景色が画面に現れたら処理をする
-        color.on('inview', function(){
-            if(counter == 0){
-            $(this).delay(200).animate({'width':'100%'},700,function(){
-                img.css('opacity','1');
-                $(this).css({'left':'0' , 'right':'auto'});
-                $(this).animate({'width':'0%'},700);
-                    });
-                counter = 1;
-            };
-        });
-    });
+    //     img.css('opacity','0');
+    //     color.css('width','0%');
+    //     //inviewを使って背景色が画面に現れたら処理をする
+    //     color.on('inview', function(){
+    //         if(counter == 0){
+    //         $(this).delay(200).animate({'width':'100%'},700,function(){
+    //             img.css('opacity','1');
+    //             $(this).css({'left':'0' , 'right':'auto'});
+    //             $(this).animate({'width':'0%'},700);
+    //                 });
+    //             counter = 1;
+    //         };
+    //     });
+    // });
 
     // fv超えたら色が変わる
     $(function () {
