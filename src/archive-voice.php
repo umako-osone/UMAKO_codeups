6ae25b7ca5a338b8438382c5413a0d1a1402421c
あなたの名前 <umako0720@gmail.com>
2023-10-04 15:12:37 +0900

    wordpress

---
+++ src/archive-voice.php
@@ -0,0 +1,150 @@
+<?php get_header(); ?>
+
+      <!--　ボイス -->
+      <section class="archive-voice">
+        <div class="archive-voice__mv mv">
+          <h1 class="mv__heading heading">voice</h1>
+        </div>
+        <div class="archive-voice__inner inner">
+          <div class="archive-voice__breadcrumb breadcrumb inner">
+
+            <!-- パンくず -->
+          <?php get_template_part('parts/breadcrumb'); ?>
+
+          </div>
+          <div class="archive-voice__content">
+            <div class="archive-voice__tab tab">
+              <a href="" class="tab__category js-tab__category is-active">all</a>
+              <a href="" class="tab__category js-tab__category">ライセンス講習</a>
+              <a href="" class="tab__category js-tab__category">ファンダイビング</a>
+              <a href="" class="tab__category js-tab__category">体験ダイビング</a>
+            </div>
+            <div class="archive-voice__items voice-card">
+              <ul class="voice-card__list">
+                <li class="voice-card__items">
+                  <a href="archive-voice.html">
+                    <div class="voice-card__item">
+                      <div class="voice-card__title-block">
+                        <div class="voice-card__categories">
+                          <p class="voice-card__gender">20代(女性)</p>
+                          <span class="voice-card__category">ライセンス講習</span>
+                        </div>
+                        <div class="voice-card__title">
+                          <h3>ここにタイトルが入ります。ここにタイトル</h3>
+                        </div>
+                      </div>
+                      <figure class="voice-card__img">
+                        <img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/voice1.jpg" alt="女性の画像">
+                      </figure>
+                    </div>
+                    <p class="voice-card__text">ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>ここにテキストが入ります。ここにテキストが入ります。</p>
+                  </a>
+                </li>
+                <li class="voice-card__items">
+                  <a href="archive-voice.html">
+                    <div class="voice-card__item">
+                      <div class="voice-card__title-block">
+                        <div class="voice-card__categories">
+                          <p class="voice-card__gender">30代(男性)</p>
+                          <span class="voice-card__category">ファンダイビング</span>
+                        </div>
+                        <div class="voice-card__title">
+                          <h3>ここにタイトルが入ります。ここにタイトル</h3>
+                        </div>
+                      </div>
+                      <figure class="voice-card__img">
+                        <img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/voice2.jpg" alt="男性の画像">
+                      </figure>
+                    </div>
+                    <p class="voice-card__text">ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>ここにテキストが入ります。ここにテキストが入ります。</p>
+                  </a>
+                </li>
+                <li class="voice-card__items">
+                  <a href="archive-voice.html">
+                    <div class="voice-card__item">
+                      <div class="voice-card__title-block">
+                        <div class="voice-card__categories">
+                          <p class="voice-card__gender">30代(女性)</p>
+                          <span class="voice-card__category">体験ダイビング</span>
+                        </div>
+                        <div class="voice-card__title">
+                          <h3>ここにタイトルが入ります。ここにタイトル</h3>
+                        </div>
+                      </div>
+                      <figure class="voice-card__img">
+                        <img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/voice3.jpg" alt="女性2人の画像">
+                      </figure>
+                    </div>
+                    <p class="voice-card__text">ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>ここにテキストが入ります。ここにテキストが入ります。</p>
+                  </a>
+                </li>
+                <li class="voice-card__items">
+                  <a href="archive-voice.html">
+                    <div class="voice-card__item">
+                      <div class="voice-card__title-block">
+                        <div class="voice-card__categories">
+                          <p class="voice-card__gender">20代(女性)</p>
+                          <span class="voice-card__category">体験ダイビング</span>
+                        </div>
+                        <div class="voice-card__title">
+                          <h3>ここにタイトルが入ります。ここにタイトル</h3>
+                        </div>
+                      </div>
+                      <figure class="voice-card__img">
+                        <img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/voice4.jpg" alt="女性の画像">
+                      </figure>
+                    </div>
+                    <p class="voice-card__text">ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>ここにテキストが入ります。ここにテキストが入ります。</p>
+                  </a>
+                </li>
+                <li class="voice-card__items">
+                  <a href="archive-voice.html">
+                    <div class="voice-card__item">
+                      <div class="voice-card__title-block">
+                        <div class="voice-card__categories">
+                          <p class="voice-card__gender">30代(カップル)</p>
+                          <span class="voice-card__category">ファンダイビング</span>
+                        </div>
+                        <div class="voice-card__title">
+                          <h3>ここにタイトルが入ります。ここにタイトル</h3>
+                        </div>
+                      </div>
+                      <figure class="voice-card__img">
+                        <img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/voice5.jpg" alt="男性と女性の画像">
+                      </figure>
+                    </div>
+                    <p class="voice-card__text">ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>ここにテキストが入ります。ここにテキストが入ります。</p>
+                  </a>
+                </li>
+                <li class="voice-card__items">
+                  <a href="archive-voice.html">
+                    <div class="voice-card__item">
+                      <div class="voice-card__title-block">
+                        <div class="voice-card__categories">
+                          <p class="voice-card__gender">20代(女性)</p>
+                          <span class="voice-card__category">ライセンス取得</span>
+                        </div>
+                        <div class="voice-card__title">
+                          <h3>ここにタイトルが入ります。ここにタイトル</h3>
+                        </div>
+                      </div>
+                      <figure class="voice-card__img">
+                        <img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/voice6.jpg" alt="女性の画像">
+                      </figure>
+                    </div>
+                    <p class="voice-card__text">ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>ここにテキストが入ります。ここにテキストが入ります。</p>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <!-- ページネーション -->
+            <div class="wp-pagenavi archive-voice__pagenavi">
+              <?php wp_pagenavi(); ?>
+            </div>
+          </div>
+        </div>
+      </section>
+    <!-- コンタクト -->
+    <?php get_template_part('parts/contact'); ?>
+
+  <?php get_footer(); ?>
--- src/page-price.php
+++ src/page-price.php
@@ -7,6 +7,7 @@
         </div>
         <div class="page-price__inner inner">
 
+        <!-- パンくず -->
         <?php get_template_part('parts/breadcrumb'); ?>
 
           <div class="page-price__content">
@@ -97,43 +98,8 @@
           </div>
         </div>
       </section>
+
       <!-- コンタクト -->
-      <section class="contact top-contact">
-        <div class="contact__inner inner">
-          <div class="contact__body">
-            <div class="contact__items">
-              <figure class="contact__logo">
-                <img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/logo-green.svg" alt="コードアップス">
-              </figure>
-              <div class="contact__info">
-                <ul class="contact__item">
-                  <li class="contact__address">沖縄県那覇市1-1</li>
-                  <li class="contact__tel">
-                    <a href="tel:01200000000">tel:0120-000-0000</a>
-                  </li>
-                  <li>営業時間:8:30-19:00</li>
-                  <li>定休日:毎週火曜日
-                  </li>
-                </ul>
-                <div class="contact__map">
-                  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d19016.32624698157!2d127.67060615241546!3d26.222643361682515!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sja!2sjp!4v1688689534890!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                </div>
-              </div>
-            </div>
-            <div class="contact__access">
-              <div class="contact__title title">
-                <div class="title__inner">
-                  <p class="title-en">contact</p>
-                  <h2 class="title-ja">お問い合わせ</h2>
-                  <p class="title-sub">ご予約・お問い合わせはコチラ</p>
-                  <div class="contact__btn">
-                    <a href="#" class="btn">contact&ensp;us<span></span></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <?php get_template_part('parts/contact'); ?>
 
       <?php get_footer(); ?>
